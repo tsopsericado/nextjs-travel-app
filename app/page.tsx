d4786ba3511e7ed00743a98fe3fc95c31f39d65c
ricardo <ricardotsopse@gmail.com>
2024-01-28 16:00:11 +0100

    working on campsite on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import Image from"next/image";
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  );
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  )
 }
